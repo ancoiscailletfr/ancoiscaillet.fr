95ff163b7dd8528e9569f3b464cecc4125b91cca
François Caillet <contact@ancoiscaillet.fr>
2021-12-11 15:08:58 +0100

    chore: upgrade next-pwa & react

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = (phase, { defaultConfig }) =>
-  withPWA({
-    pwa: {
-      disable: phase === PHASE_DEVELOPMENT_SERVER,
-      dest: 'public',
-      runtimeCaching
-    }
-  })
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  }
+})
